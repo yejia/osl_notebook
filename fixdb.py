90e97c20578a9ec3ec5d365005da6d496cc6310d
Beyond Liu <beyond.leon.liu@gmail.com>
2012-02-16 15:16:16 +0800

    trivial

--- fixdb.py
+++ fixdb.py
@@ -30,29 +30,6 @@ from django.contrib.auth.models import User
 
 
 
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
-
-
-from django.utils.translation import ugettext_noop as _
-from django.db.models import signals
-
-if "notification" in settings.INSTALLED_APPS:
-    from notification import models as notification
-
-    def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("friends_invite", _("Invitation Received"), _("you have received an invitation"))
-        notification.create_notice_type("friends_accept", _("Acceptance Received"), _("an invitation you sent has been accepted"))
-        notification.create_notice_type("comment_receive", _("Comment Received"), _("you have received a comment"))
-        
-
-    signals.post_syncdb.connect(create_notice_types, sender=notification)
-else:
-    print "Skipping creation of NoticeTypes as notification app not found"
 
 
 
